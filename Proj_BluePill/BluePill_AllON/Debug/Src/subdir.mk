219c851362716701035030bf3cbdc245d17b7266
ssserghei <ssserghei@gmail.com>
2021-12-15 23:18:20 +0200

    reinstail cube

--- Proj_BluePill/BluePill_AllON/Debug/Src/subdir.mk
+++ Proj_BluePill/BluePill_AllON/Debug/Src/subdir.mk
@@ -18,8 +18,13 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/main.o: ../Src/main.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -DDEBUG -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/main.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
-Src/sysmem.o: ../Src/sysmem.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -DDEBUG -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/sysmem.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Src/%.o: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -DDEBUG -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/main.d ./Src/main.o ./Src/sysmem.d ./Src/sysmem.o
+
+.PHONY: clean-Src
 
--- Proj_BluePill/BluePill_AllON/Debug/Startup/subdir.mk
+++ Proj_BluePill/BluePill_AllON/Debug/Startup/subdir.mk
@@ -15,6 +15,13 @@ S_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Startup/startup_stm32f103c8tx.o: ../Startup/startup_stm32f103c8tx.s Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -x assembler-with-cpp -MMD -MP -MF"Startup/startup_stm32f103c8tx.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+Startup/%.o: ../Startup/%.s Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+
+clean: clean-Startup
+
+clean-Startup:
+	-$(RM) ./Startup/startup_stm32f103c8tx.d ./Startup/startup_stm32f103c8tx.o
+
+.PHONY: clean-Startup
 
