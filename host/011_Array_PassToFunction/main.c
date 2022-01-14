e4f90122eb82d3ae2f6a8bf90031c4133bb832db
ssserghei <ssserghei@gmail.com>
2022-01-14 18:21:17 +0200

    1

--- host/011_Array_PassToFunction/main.c
+++ host/011_Array_PassToFunction/main.c
@@ -4,24 +4,26 @@
  *  Created on: 13 янв. 2022 г.
  *      Author: SergheiSGarage
  */
-
 #include<stdio.h>
 #include<stdint.h>
 
 void array_display(uint8_t const *const pArray,uint32_t nitems);
 
 int main(void){
-	uint8_t someData[]={0xf0,0xf1,0xf2,0xf3,0xf4,0xf5,0xf6,0xf7,0xf8,0xf9,0xfA};	//compilatorul singur calculeaza marimea tabloului
+	uint8_t someData[]={0xf0,0xf1,0xf2,0xf3,0xf4,0xf5,0xf6,0xf7,0xf8,0xf9};	//compilatorul singur calculeaza marimea tabloului
 
 	printf("Before : 2nd data item = %x\n",*(someData+1)); fflush(stdout);
-
+/*
 	for(uint32_t i=0; i<10; i++){
-		someData[1]=0x33;
+		someData[i]=i;
 	}
-
+*/
 	uint32_t nItems=sizeof(someData)/sizeof(uint8_t);
-	array_display(someData,nItems);
 
+	printf("sizeof(someData)= %d\n",sizeof(someData));
+	printf("sizeof(uint8_t)= %d\n",sizeof(uint8_t));
+
+	array_display((someData+2),nItems-3);
 
 	fflush(stdout);
 	while(getchar() != '\n');
@@ -32,6 +34,6 @@ int main(void){
 
 void array_display(uint8_t const *const pArray,uint32_t nItems){
 	for(uint32_t i=0; i<nItems; i++){
-	printf("= %x\n",*(pArray+1)); fflush(stdout);
+	printf("= %x\n",*(pArray+i)); fflush(stdout);
 	}//end for
 }//end main
---
+++ host/012_Array_Swap/main.c
@@ -0,0 +1,29 @@
+/*
+ * main.c
+ *
+ *  Created on: 14 янв. 2022 г.
+ *      Author: SergheiSGarage
+ */
+
+#include<stdio.h>
+#include<stdint.h>
+
+
+void main(void){
+
+	array1[5];
+	array2[5];
+
+	printf("introdu elementele primului tablou");
+	scanf();
+
+
+
+
+
+
+
+
+
+
+}//end main
