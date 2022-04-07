5aae5c7052f8ec8007e9bdd7a0143ff0acd91432
ssserghei <ssserghei@gmail.com>
2022-04-07 08:57:07 +0300

    1

--- Proj_BlackPill_STM32L151CCU6/001_BlPil_L151CCU_InterPriority/Src/main.c
+++ Proj_BlackPill_STM32L151CCU6/001_BlPil_L151CCU_InterPriority/Src/main.c
@@ -19,12 +19,6 @@
 #include <stdint.h>
 #include<stdio.h>
 
-uint32_t *pNVIC_IPRBase = (uint32_t*) 0xE000E400;	//Interrupt Priority Registers on page 4-7
-uint32_t *pNVIC_ISERBase = (uint32_t*) 0xE000E100;	//Interrupt Set-enable Registers on page 4-4
-uint32_t *pNVIC_ISPRBase = (uint32_t*) 0xE000E200;	//Interrupt Set-pending Registers on page 4-5
-
-void configure_priority_for_irqs(uint8_t, uint8_t);
-
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
@@ -32,7 +26,33 @@ void configure_priority_for_irqs(uint8_t, uint8_t);
 #define IRQNO_TIMER2	28		//Position
 #define IRQNO_I2C1		31
 
-/*==================================================*/
+uint32_t *pNVIC_IPRBase = (uint32_t*) 0xE000E400;	//Interrupt Priority Registers on page 4-7
+uint32_t *pNVIC_ISERBase = (uint32_t*) 0xE000E100;	//Interrupt Set-enable Registers on page 4-4
+uint32_t *pNVIC_ISPRBase = (uint32_t*) 0xE000E200;	//Interrupt Set-pending Registers on page 4-5
+
+/*=========================================================*/
+void configure_priority_for_irqs(uint8_t irq_no, uint8_t priority_value){
+
+		uint8_t iprx=irq_no/4;
+		uint32_t *ipr=pNVIC_IPRBase+iprx;
+
+		//2. position in iprx
+		uint8_t pos=(irq_no %4)*8;
+
+		//3. configure the priority
+		*ipr &=~(0xFF <<pos);	//clear
+		*ipr |= (priority_value << pos);
+}
+/*	76543210	4.2.7 Interrupt Priority Registers
+	--------
+	| PRI_0 | interrupt priority register implementation
+	--------
+	  8 bits
+	   IRQ0
+*/
+	//1. find out iprx
+/* Берем номер IRQ и делим на 4 (потому что в каждом регистре 4 IRQ )....*/
+/*========================================================*/
 int main(void)
 {
 	//1. Configure the priority for the peripherals
@@ -45,39 +65,19 @@ int main(void)
 	//3. Enable the IRQs in NVIC ISER
 	*pNVIC_ISERBase |=(1<<IRQNO_I2C1);
 	*pNVIC_ISERBase |=(1<<IRQNO_TIMER2);
-//	while(1);
 }
-/*==================================================*/
+
+/*=========================================================*/
 void TIM2_IRQHandler(void){        /* TIM2 global interrupt        */
 	printf("[TIM2_IRQHandler]\n");
 	*pNVIC_ISPRBase |=(1<<IRQNO_I2C1);
-//	while(1);
+	while(1);
 }
-/*==================================================*/
+
+/*=========================================================*/
 void I2C1_EV_IRQHandler(void){     /* I2C1 event interrupt     */
 	printf("[I2C1_EV_IRQHandler]\n");
-//	while(1);
+	while(1);
 }
 
-/*==================================================*/
-void configure_priority_for_irqs(uint8_t irq_no, uint8_t priority_value){
-	/*	76543210	4.2.7 Interrupt Priority Registers
-		--------
-		| PRI_0 | interrupt priority register implementation
-		--------
-		  8 bits
-		   IRQ0
-	*/
-		//1. find out iprx
-/* Берем номер IRQ и делим на 4 (потому что в каждом регистре 4 IRQ )
- */
-		uint8_t iprx=irq_no/4;
-		uint32_t *ipr=pNVIC_IPRBase+iprx;
 
-		//2. position in iprx
-		uint8_t pos=(irq_no %4)*8;
-
-		//3. configure the priority
-		*ipr &=~(0xFF <<pos);	//clear
-		*ipr |= (priority_value << pos);
-}
