babe1f0b4ac311fbf8b6e63e547d3094b9fc15aa
ssserghei <ssserghei@gmail.com>
2022-09-26 15:23:04 +0300

    022  Begin. Sa compilat

---
+++ Proj_NUCLEO-F446RE/022_stm32f4xx_drivers_noAPI/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ D:/GitHub/EmbedSoftProjects/Proj_NUCLEO-F446RE/022_stm32f4xx_drivers_noAPI/drivers/Inc/stm32f446xx.h \
+ D:/GitHub/EmbedSoftProjects/Proj_NUCLEO-F446RE/022_stm32f4xx_drivers_noAPI/drivers/Inc/stm32f446xx_gpio_driver.h \
+ D:/GitHub/EmbedSoftProjects/Proj_NUCLEO-F446RE/022_stm32f4xx_drivers_noAPI/drivers/Inc/stm32f446xx.h
+D:/GitHub/EmbedSoftProjects/Proj_NUCLEO-F446RE/022_stm32f4xx_drivers_noAPI/drivers/Inc/stm32f446xx.h:
+D:/GitHub/EmbedSoftProjects/Proj_NUCLEO-F446RE/022_stm32f4xx_drivers_noAPI/drivers/Inc/stm32f446xx_gpio_driver.h:
+D:/GitHub/EmbedSoftProjects/Proj_NUCLEO-F446RE/022_stm32f4xx_drivers_noAPI/drivers/Inc/stm32f446xx.h:
---
+++ Proj_NUCLEO-F446RE/022_stm32f4xx_drivers_noAPI/Debug/drivers/Src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/Src/stm32f446xx_gpio_driver.c 
+
+OBJS += \
+./drivers/Src/stm32f446xx_gpio_driver.o 
+
+C_DEPS += \
+./drivers/Src/stm32f446xx_gpio_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+drivers/Src/%.o drivers/Src/%.su: ../drivers/Src/%.c drivers/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F446RETx -c -I"D:/GitHub/EmbedSoftProjects/Proj_NUCLEO-F446RE/022_stm32f4xx_drivers_noAPI/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-drivers-2f-Src
+
+clean-drivers-2f-Src:
+	-$(RM) ./drivers/Src/stm32f446xx_gpio_driver.d ./drivers/Src/stm32f446xx_gpio_driver.o ./drivers/Src/stm32f446xx_gpio_driver.su
+
+.PHONY: clean-drivers-2f-Src
+
